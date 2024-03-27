913ee8aab1a89fef58a8470fb702625a067c6236
soj <josellecallora08@gmail.com>
2024-03-27 16:16:37 +0800

    added charts

--- pages/dashboard/index.page.jsx
+++ pages/dashboard/index.page.jsx
@@ -1,12 +1,10 @@
+import Dashboard from "../../components/dashboard/Dashboard"
 export { Page }
 
 function Page() {
   return (
     <>
-      <h1>Dashboard</h1>
-      <p>
-        Example of using <code>vite-plugin-ssr</code>.
-      </p>
+      <Dashboard/>
     </>
   )
 }
