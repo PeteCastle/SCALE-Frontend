594c304f8d57816cd9fb4d25b36e40df591de647
Soj <josellecallora08@gmail.com>
2024-03-28 14:12:06 +0800

    fixed height

--- components/dashboard/Dashboard.jsx
+++ components/dashboard/Dashboard.jsx
@@ -6,15 +6,15 @@ import Fumigation from "./Fumigation";
 
 const Dashboard = () => {
   return (
-    <section className="w-full h-full min-h-screen">
+    <section className="w-full h-full min-h-screen ">
       <div className="h-full w-11/12 m-auto">
         <h1 className="font-bold text-2xl py-5">Live Summary</h1>
-        <div className="h-full grid grid-cols-auto grid-rows-2 gap-5 pb-5">
-          <div className="col-span-1 row-span-1 grid grid-cols-1 md:grid-cols-5 gap-5">
+        <div className="h-full flex flex-col md:grid grid-cols-auto grid-rows-2 gap-5 pb-5">
+          <div className="h-full col-span-1 row-span-1 grid grid-cols-1 md:grid-cols-5 gap-5">
             <Summary />
             <Operation />
           </div>
-          <div className="col-span-1 row-span-1 flex flex-col">
+          <div className="h-full col-span-1 row-span-1 flex flex-col pb-5 md:pb-0">
             <h1 className="py-5 font-bold text-2xl">Statistics</h1>
             <div className="h-full grid grid-cols-1 md:grid-cols-2 gap-5">
               <Detected />
