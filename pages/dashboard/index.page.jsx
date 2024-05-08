d9eeb264afd8db9a5d90563e435c1629c8b1b222
soj <josellecallora08@gmail.com>
2024-05-08 16:46:26 +0800

    added footer in live dashboard

--- pages/dashboard/index.page.jsx
+++ pages/dashboard/index.page.jsx
@@ -1,10 +1,12 @@
 import Dashboard from "../../components/dashboard/Dashboard"
+import Footer from "../../components/index/Footer"
 export { Page }
 
 function Page() {
   return (
     <>
       <Dashboard/>
+      <Footer/>
     </>
   )
 }
